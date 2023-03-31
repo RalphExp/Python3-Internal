28860061fc913ec2dc29b97da62dbbc561cf25ff
ralph <ralphxwz@gmail.com>
2023-03-31 22:10:08 +0800

    update

--- Parser/reg.py
+++ Parser/reg.py
@@ -3,6 +3,82 @@
 # This file shows how to use NFA to create a regular 
 # expression engine.
 
+class Token(object):
+    END = 0
+    ALTER = 1
+    LPAR = 2
+    RPAR = 3
+    STAR = 4
+    STAR2 = 5
+    PLUS = 6
+    PLUS2 = 7
+    QUESTION = 8
+    CHAR = 9
+    LBRACK = 10
+    RBRACK = 11
+    BACKSLASH = 12
+    LBRACE = 13
+    RBRACE = 14
+
+    def __init__(self, type, value=None):
+        self.type = type
+        self.value = value
+
+class Tokenizer(object):
+
+    def __init__(self, pattern):
+        self._pat = pattern
+        self._cache = None
+        self._index = 0
+
+        self._token_dict = {
+            '|': Token(Token.ALTER),
+            '(': Token(Token.LPAR),
+            ')': Token(Token.RPAR),
+            '[': Token(Token.LBRACK),
+            ']': Token(Token.RBRACK),
+            '{': Token(Token.LBRACE),
+            '}': Token(Token.RBRACE),
+            '*': Token(Token.STAR),
+            '+': Token(Token.PLUS),
+            '?': Token(Token.QUESTION),
+        }
+
+        self._token2_dict = {
+            '*?': Token(Token.STAR2),
+            '+?': Token(Token.PLUS2)
+        }
+
+    def putToken(self, token):
+        assert(self._cache is None)
+        self._cache = token
+
+    def getToken(self):
+        if self._cache:
+            t = self._cache 
+            self._cache = None
+            return t
+        
+        if self._index >= len(self._pat):
+            return Token(Token.END, None)
+
+        s = self._pat
+        tok = self._token2_dict.get(s[self._index:self._index+2], None) 
+        if tok != None:
+            self._index += 2
+            return tok
+        
+        tok = self._token_dict.get(s[self._index], None)
+        if tok != None:
+            self._index += 1
+            return tok
+        
+        tok = Token(Token.CHAR, s[self._index])
+        self._index += 1
+        return tok
+
+
+
 class NFAArc(object):
     """ NFAArc represent the arcs connecting to the nextN States,
     value is diffent according to different type. if type is 
@@ -73,6 +149,7 @@ class NFA(object):
         self._start = -1
         self._end = -1
         self._nodes = []
+        self._groups = 0
     
     def allocState(self) -> NFAState:
         state = NFAState()
@@ -100,8 +177,12 @@ class RegExp(object):
         self._compiled = False
         self._nfa = compile()
         self._compiled = True
+        self._tokenizer = Tokenizer(self._pat)
 
     def concat(self) -> tuple[NFAState]:
+        pass
+
+    def group(self) -> tuple[NFAState]:
         """ concat single character,class,groups,closure,star,... etc.
         """
         s = self._pat
@@ -109,18 +190,30 @@ class RegExp(object):
 
         while self._index < len(s):
             if s[self._index] == '(':
+                self._nfa.groups += 1
                 self._index += 1
+                group = self._nfa.groups
+
                 a, z = self.alternate()
                 if s[self._index] != ')':
                     raise Exception('unmatch parenthesis')
                 aa = self._nfa.allocState()
                 zz = self._nfa.allocState()
                 
+                aa.addArc(a, group, NFAArc.LPAR_TYPE)
+                z.addArc(zz, group, NFAArc.RPAR_TYPE)
+                return aa, zz
+            else:
+                a, z = self.concat()
+                if self._index < len(s):
+                    if s[self._index] == '*':
+                        pass
+                
     def alternate(self) -> tuple[NFAState]:
         """ alternate split s into different section delimit by '|'
         """
         s = self._pat
-        a, z = self.concat()
+        a, z = self.group()
         
         if self._index == len(s) or s[self._index] != '|':
             return a, z
