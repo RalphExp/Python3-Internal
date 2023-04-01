0be130c5921932f8ff5d7b5adb37b0751feb97e2
ralph <ralphxwz@gmail.com>
2023-04-01 13:45:10 +0800

    update

--- Parser/re2.py
+++ Parser/re2.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 
-# This file shows how to use NFA to create a regular 
-# expression engine.
+# This file shows how to use NFA to construct a regular expression engine.
+
+from __future__ import annotations
+
+import pdb
 
 class Token(object):
     END = 0
@@ -29,14 +32,12 @@ class Token(object):
     def __repr__(self) -> str:
         return f'token: type = {self.type}, val = {self.value}, pos = {self.pos}'
 
-class Tokenizer(object):
 
+class Tokenizer(object):
     def __init__(self, pattern):
         self._pat = pattern
         self._token = None
         self._index = 0
-        self.next()
-
         self._token_dict = {
             '|': Token(Token.ALTER, '|'),
             '(': Token(Token.LPAREN, '('),
@@ -49,14 +50,19 @@ class Tokenizer(object):
             '+': Token(Token.PLUS, '+'),
             '?': Token(Token.QUEST, '?'),
         }
-
         self._token2_dict = {
             '*?': Token(Token.STAR2, '*?'),
             '+?': Token(Token.PLUS2, '+?'),
             '??': Token(Token.QUEST2, '??')
         }
+        self.next()
 
-    def getToken(self):  
+    @property
+    def index(self):
+        return self._index
+
+    @property
+    def token(self):  
         return self._token
     
     def next(self):
@@ -68,18 +74,21 @@ class Tokenizer(object):
         if token != None:
             token.pos = self._index
             self._index += 2
+            self._token = token
             return token
         
         token = self._token_dict.get(s[self._index], None)
         if token != None:
             token.pos = self._index
             self._index += 1
+            self._token = token
             return token
         
         token = Token(Token.CHAR, s[self._index], self._index)
         self._index += 1
         self._token = token
         return self._token
+    
 
 class NFAArc(object):
     """ NFAArc represent the arcs connecting to the nextN States,
@@ -96,7 +105,7 @@ class NFAArc(object):
     LGROUP = 3
     RGROUP = 4
 
-    def __init__(self, target, value, type_):
+    def __init__(self, target:NFAState, value, type_):
         self._type = type_
         self._value = value
         self._target = target
@@ -132,10 +141,10 @@ class NFAState(object):
     def accept(self, value:bool):
         self.accept = value
 
-    def appendArc(self, target, value, type_):
+    def appendArc(self, target:NFAState, value, type_):
         self._arcs.append(NFAArc(target, value, type_))
 
-    def prependArc(self, target, value, type_):
+    def prependArc(self, target:NFAState, value, type_):
         assert(len(self._arcs) == 1)
         self._arcs.insert(0, NFAArc(target, value, type_))
 
@@ -168,7 +177,7 @@ class NFA(object):
         self._nodes.append(state)
         return state
     
-    def dump(self, debug):
+    def dump(self, debug:bool):
         """Dump a graphical representation of the NFA"""
         todo = [self.start]
         for i, state in enumerate(todo):
@@ -200,19 +209,16 @@ class RegExp(object):
     note that is this different from pgen's NFA, we want to 
     make it more intuitive
     """
-    def __init__(self, pattern, debug=True):
+    def __init__(self, pattern:str, debug:bool=False):
         self._pat = pattern
-        self._index = 0
         self._debug = debug
         self._tokenizer = Tokenizer(self._pat)
         self._nfa = NFA()
         self._compiled = False
-        self.compile()
-        self._compiled = True
 
     def getToken(self):
         # getToken get the current token but not consume it
-        return self._tokenizer.getToken()
+        return self._tokenizer.token
     
     def nextToken(self):
         # nextToken consumes the current one and get 
@@ -230,6 +236,9 @@ class RegExp(object):
                 z = self._nfa.newState()
                 zz.appendArc(z, token.value, Token.CHAR)
                 zz = z
+                self.nextToken()
+            elif token.type == Token.END:
+                break
             else:
                 break
 
@@ -243,9 +252,14 @@ class RegExp(object):
         aa = None # NFA State to return
         zz = None # NFA State to return
 
+        pdb.set_trace()
+
         # invariant property: len(zz._arc) == 0
         while True:
             token = self.getToken()
+            if token.type in (Token.ALTER, Token.END):
+                break
+
             if token.type == Token.LPAREN:
                 self._nfa.groups += 1
                 self._index += 1
@@ -256,7 +270,7 @@ class RegExp(object):
 
                 token = self.getToken()
                 if token.type != Token.RPAREN:
-                    raise Exception('unmatch parenthesis')
+                    raise Exception('Unmatch parenthesis')
                 self.nextToken() # consume ')'
 
                 if aa == None:
@@ -303,13 +317,19 @@ class RegExp(object):
                 elif token.type == Token.QUEST2:
                     self.nextToken()
                     a.prependArc(z, None, NFAArc.EPSILON)
-                elif token.type == Token.ALTER:
+                elif token.type in (Token.ALTER, Token.END):
+                    # if meet the ALTER token, should return and let alternate
+                    # to handle it, but if aa is None, we should skip it and
+                    # continue to parse.
+                    if token.type == Token.ALTER and aa is None:
+                        self.nextToken()
+                        continue
                     if aa is not None:
                         zz.appendArc(a, None, NFAArc.EPSILON)
                         zz = z
                     else:
                         aa, zz = a, z
-                    return aa, zz
+                    break
                
                 # not allow ++/**/*+/+*/... etc
                 idx = self._index
@@ -327,46 +347,54 @@ class RegExp(object):
                     zz = z
                 assert(len(zz._arc) == 0)
 
+        #end while
+        return aa, zz
+
+
     def alternate(self) -> tuple[NFAState]:
         """ alternate split s into different section delimited by '|'
         """
-        s = self._pat
+        pdb.set_trace()
+
         a, z = self.group()
-        
+
         token = self.getToken()
-        if token.value != Token.ALTER:
+        if token.type != Token.ALTER:
             return a, z
  
-        self.nextToken()
         aa = self._nfa.newState()
         zz = self._nfa.newState()
 
         while True:
             aa.appendArc(a, None, NFAArc.EPSILON)
             z.appendArc(zz, None, NFAArc.EPSILON)
+
             token = self.getToken()
-            if token.value != Token.ALTER:
+            if token.type != Token.ALTER:
                 break
 
             self.nextToken()
-            self._index += 1
-            a, z = self.group(s)
+            a, z = self.group()
         return aa, zz
 
-    def compile(self) -> NFA:
+    def compile(self) -> None:
+        if self._compiled:
+            return
+        
         s = self._pat
         start, end = self.alternate()
-        if self._index != len(s):
-            raise Exception(f'Unexpected character: {s[self._index]} as position {self._index}')
+        if self._tokenizer.index != len(s):
+            raise Exception(f'Unexpected token: {self.getToken()}')
 
         end.accept = True
         self._nfa.start = start
         self._nfa.end = end
         self._nfa.dump(self._debug)
+        self._compiled = True
 
     def match(text) -> Group or None:
         pass
 
 if __name__ == '__main__':
-    re = RegExp('ab|cd|ef')
+    re = RegExp('ab|cd|ef', debug=True)
     re.compile()
